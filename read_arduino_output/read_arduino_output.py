5f38d05a234054d6df15141d0b431f8d2ef129b1
GabrielHCaribe <gabe.h.c@hotmail.com>
2024-03-19 23:40:26 -0400

    Added file to read arduino output into python file through a USB cable

---
+++ read_arduino_output/read_arduino_output.py
@@ -0,0 +1,23 @@
+import serial
+import time
+
+def main_fcn():
+    arduino = serial.Serial("com6", 9600)
+    print("Established Serial Connection to Arduino")
+    while True:
+        arduino_data = arduino.readline()
+        decoded_values = str(arduino_data[0:len(arduino_data)].decode("utf-8"))
+        # list_values = decoded_values.split(" | ")
+
+        print(f"Collected readings from Arduino: {list_values}")
+
+        print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+
+'''_________________________________________________________________'''
+
+list_values = []
+list_in_floats = []
+
+print("Program Started")
+
+main_fcn()
